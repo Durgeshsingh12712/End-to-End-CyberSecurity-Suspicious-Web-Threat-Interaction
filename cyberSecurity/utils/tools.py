95c252bbeedc98e21a3e3ce46656bd41e1d1c7b9
unknown <durgeshsingh12712@gmail.com>
2025-08-08 13:04:58 +0530

    Tool

---
+++ cyberSecurity/utils/tools.py
@@ -0,0 +1,65 @@
+import os, yaml, json, pickle
+from pathlib import Path
+from box import ConfigBox
+from box.exceptions import BoxValueError
+from ensure import ensure_annotations
+
+from cyberSecurity.loggers import logger
+
+@ensure_annotations
+def read_yaml(path_to_yaml: Path) -> ConfigBox:
+    try:
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"Yaml File: {path_to_yaml} loaded successfully")
+            return ConfigBox(content)
+    except BoxValueError:
+        raise ValueError("Yaml file is empty")
+    except Exception as e:
+        raise e
+
+
+@ensure_annotations
+def create_directories(path_to_directories: list, verbose=True):
+    for path in path_to_directories:
+        os.makedirs(path, exist_ok=True)
+        if verbose:
+            logger.info(f"Created Directory at : {path}")
+
+@ensure_annotations
+def save_json(path: Path, data: dict):
+    with open(path, "w") as f:
+        json.dump(data, f, indent=4)
+    
+    logger.info(f"Json file saved at: {path}")
+
+@ensure_annotations
+def load_json(path: Path) -> ConfigBox:
+    with open(path) as f:
+        content = json.load(f)
+    
+    logger.info(f"Json file loaded successfully from: {path}")
+    return ConfigBox(content)
+
+def save_bin(data, path: str):
+    try:
+        dir_path = os.path.dirname(path)
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(path, 'wb') as f:
+            pickle.dump(data, f)
+    
+    except Exception as e:
+        raise e
+
+@ensure_annotations
+def load_bin(path: Path):
+    with open(path, 'rb') as f:
+        data = pickle.load(f)
+    logger.info(f"Binary file loaded from: {path}")
+    return data
+
+@ensure_annotations
+def get_size(path: Path) -> str:
+    size_in_kb = round(os.path.getsize(path)/1024)
+    return f"~{size_in_kb} KB"
